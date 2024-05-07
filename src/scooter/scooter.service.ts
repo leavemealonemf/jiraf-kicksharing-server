090ff13a008f4c5d1be5e2605e33073b5c1cacb4
leavemealonemf <strangemisterio78@gmail.com>
2024-05-07 14:46:08 +0300

    a

--- src/scooter/scooter.service.ts
+++ src/scooter/scooter.service.ts
@@ -234,10 +234,7 @@ export class ScooterService {
     const response = [];
 
     for (const scooter of scooters) {
-      if (scooter.rented) return;
-
       for (const rightechScooter of res) {
-        if (!rightechScooter.state.online) return;
         if (scooter.deviceIMEI === rightechScooter.id) {
           response.push({
             scooter: scooter,
