11237e39807ef2d0128c69e465bbb90ae16d53c5
leavemealonemf <strangemisterio78@gmail.com>
2024-06-02 13:48:38 +0300

    fix: scooter dto

--- src/scooter/dto/create-scooter.dto.ts
+++ src/scooter/dto/create-scooter.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { $Enums } from '@prisma/client';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateScooterDto {
   @ApiProperty({ default: 1 })
@@ -8,37 +8,42 @@ export class CreateScooterDto {
     message: 'Поле не должно быть пустым',
   })
   modelId: number;
+
   @ApiProperty({ default: '1231241324124212' })
-  // @IsNotEmpty({
-  //   message: 'Поле не должно быть пустым',
-  // })
-  // serialNumber: string;
   @IsNotEmpty({
     message: 'Поле не должно быть пустым',
   })
   deviceIMEI: string;
+
   @ApiProperty({ default: 100 })
   @IsNotEmpty({
     message: 'Поле не должно быть пустым',
   })
   batteryLevel: number;
+
   @ApiProperty({ default: 'ACTIVE' })
   @IsNotEmpty({
     message: 'Поле не должно быть пустым',
   })
   status: $Enums.ScooterStatus;
+
   @ApiProperty({ default: true })
   @IsNotEmpty({
     message: 'Поле не должно быть пустым',
   })
   power: boolean;
+
   @ApiProperty({ default: new Date() })
   @IsNotEmpty({
     message: 'Поле не должно быть пустым',
   })
   addedDate: Date;
+
   @ApiProperty({ default: null })
   photo?: string;
 
+  @ApiProperty({ default: 1 })
+  @IsNumber(undefined, { message: 'Ожидалось число' })
+  @IsNotEmpty({ message: 'Поле franchiseId не должно быть пустым' })
   franchiseId: number;
 }
