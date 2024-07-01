06bf17d45da1e979baf9c0040029ba4b9673c699
leavemealonemf <strangemisterio78@gmail.com>
2024-07-01 21:51:11 +0300

    upd: tariff dto

--- src/tariff/dto/update-tariff-orders.dto.ts
+++ src/tariff/dto/update-tariff-orders.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Tariff } from '@prisma/client';
+import { UpdateTariffDto } from './update-tariff.dto';
+import { IsArray } from 'class-validator';
 
 export class UpdateTariffOrdersDto {
+  @IsArray({ message: 'На вход ожидался массив tariffs[]' })
+  @ApiProperty({ default: [UpdateTariffDto] })
   tariffs: Tariff[];
 }
