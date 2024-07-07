e75de119acc9768778be43683828d709058d8c26
leavemealonemf <strangemisterio78@gmail.com>
2024-07-07 21:51:13 +0300

    upd: new device commands iot

--- libs/IoT/scooter/commands/commands.ts
+++ libs/IoT/scooter/commands/commands.ts
@@ -1,13 +1,25 @@
+// export const DEVICE_COMMANDS = {
+//   UNLOCK: 'sclockctrl0',
+//   LOCK: 'sclockctrl1',
+//   SET_SPEED_LIMIT_NORMAL_MODE_5: 'scsetspdlim-normal-5',
+//   SET_SPEED_LIMIT_NORMAL_MODE_10: 'scsetspdlim-normal-10',
+//   SET_SPEED_LIMIT_NORMAL_MODE_15: 'scsetspdlim-normal-15',
+//   SET_SPEED_LIMIT_NORMAL_MODE_20: 'scsetspdlim-normal-20',
+//   SET_SPEED_LIMIT_NORMAL_MODE_25: 'scsetspdlim-normal-25',
+//   START_ENGINE: 'scenginectrl-on',
+//   SHUT_DOWN_ENGINE: 'scenginectrl-off',
+// };
+
 export const DEVICE_COMMANDS = {
-  UNLOCK: 'sclockctrl0',
-  LOCK: 'sclockctrl1',
-  SET_SPEED_LIMIT_NORMAL_MODE_5: 'scsetspdlim-normal-5',
-  SET_SPEED_LIMIT_NORMAL_MODE_10: 'scsetspdlim-normal-10',
-  SET_SPEED_LIMIT_NORMAL_MODE_15: 'scsetspdlim-normal-15',
-  SET_SPEED_LIMIT_NORMAL_MODE_20: 'scsetspdlim-normal-20',
-  SET_SPEED_LIMIT_NORMAL_MODE_25: 'scsetspdlim-normal-25',
-  START_ENGINE: 'scenginectrl-on',
-  SHUT_DOWN_ENGINE: 'scenginectrl-off',
+  UNLOCK: 'unlock-dev',
+  LOCK: 'lock-dev',
+  SET_SPEED_LIMIT_NORMAL_MODE_5: 'speed-limit-norm-5',
+  SET_SPEED_LIMIT_NORMAL_MODE_10: 'speed-limit-norm-10',
+  SET_SPEED_LIMIT_NORMAL_MODE_15: 'speed-limit-norm-15',
+  SET_SPEED_LIMIT_NORMAL_MODE_20: 'speed-limit-norm-20',
+  SET_SPEED_LIMIT_NORMAL_MODE_25: 'speed-limit-norm-25',
+  START_ENGINE: 'engine-on',
+  SHUT_DOWN_ENGINE: 'engine-off',
 };
 
 export const DEVICE_COMMANDS_DYNAMIC = {
