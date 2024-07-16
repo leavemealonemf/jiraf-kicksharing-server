1feea2d3ab436516ed43e8c48037cb6d31c07cd7
leavemealonemf <strangemisterio78@gmail.com>
2024-07-16 20:53:52 +0300

    upd: client redirect

--- src/client-redirect/client-redirect.controller.ts
+++ src/client-redirect/client-redirect.controller.ts
@@ -4,6 +4,8 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import * as DeviceDetector from 'device-detector-js';
 
+type OS = 'android' | 'ios';
+
 @ApiBearerAuth()
 @ApiTags('Перенаправление на скачивание приложения')
 @Controller('/store')
@@ -15,8 +17,23 @@ export class ClientRedirectController {
       skipBotDetection: true,
     });
 
-    const device = deviceDetector.parse(req.headers['user-agent']);
-    console.log(device);
+    const os: OS = deviceDetector
+      .parse(req.headers['user-agent'])
+      .os.name.toLowerCase();
+
+    if (os === 'android') {
+      res.status(301).redirect('https://giraffe-go.ru/');
+      return;
+    }
+
+    if (os === 'ios') {
+      res
+        .status(301)
+        .redirect(
+          'https://apps.apple.com/app/%D0%B6%D0%B8%D1%80%D0%B0%D1%84-go/id6503244292',
+        );
+      return;
+    }
 
     res.status(301).redirect('https://giraffe-go.ru/');
   }
