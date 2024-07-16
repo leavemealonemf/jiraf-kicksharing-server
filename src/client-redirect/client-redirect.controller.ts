2d75ee38574608ec81c54129d95f7014efc49360
leavemealonemf <strangemisterio78@gmail.com>
2024-07-16 20:41:36 +0300

    upd: device detector

--- src/client-redirect/client-redirect.controller.ts
+++ src/client-redirect/client-redirect.controller.ts
@@ -2,7 +2,7 @@ import { Public } from '@common/decorators';
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
-import DeviceDetector from 'device-detector-js';
+import * as DeviceDetector from 'device-detector-js';
 
 @ApiBearerAuth()
 @ApiTags('Перенаправление на скачивание приложения')
@@ -11,7 +11,9 @@ export class ClientRedirectController {
   @Public()
   @Get()
   async redirectToMobileStore(@Res() res: Response, @Req() req: Request) {
-    const deviceDetector = new DeviceDetector();
+    const deviceDetector = new DeviceDetector({
+      skipBotDetection: true,
+    });
 
     const device = deviceDetector.parse(req.headers['user-agent']);
     console.log(device);
