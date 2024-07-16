a3f615fc9ce4ecf7ed19b76ca2b2b36b6a243e3b
leavemealonemf <strangemisterio78@gmail.com>
2024-07-16 20:36:20 +0300

    upd: device detector

--- src/client-redirect/client-redirect.controller.ts
+++ src/client-redirect/client-redirect.controller.ts
@@ -1,7 +1,8 @@
 import { Public } from '@common/decorators';
-import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import DeviceDetector from 'device-detector-js';
 
 @ApiBearerAuth()
 @ApiTags('Перенаправление на скачивание приложения')
@@ -10,7 +11,11 @@ export class ClientRedirectController {
   @Public()
   @Get()
   async redirectToMobileStore(@Res() res: Response, @Req() req: Request) {
-    console.log(req.headers['user-agent']);
+    const deviceDetector = new DeviceDetector();
+
+    const device = deviceDetector.parse(req.headers['user-agent']).os.name;
+    console.log(device);
+
     res.status(301).redirect('https://giraffe-go.ru/');
   }
 }
